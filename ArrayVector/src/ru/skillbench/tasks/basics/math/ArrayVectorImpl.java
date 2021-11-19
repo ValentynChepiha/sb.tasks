3706ec8a928005896fb2f5f178d324b7d4cdb4f9
Valentyn Chepiha <valentin.chepiga@gmail.com>
2021-11-19 16:57:08 +0200

    Added task

---
+++ ArrayVector/src/ru/skillbench/tasks/basics/math/ArrayVectorImpl.java
@@ -0,0 +1,84 @@
+package ru.skillbench.tasks.basics.math;
+
+public class ArrayVectorImpl implements ArrayVector {
+    @Override
+    public void set(double... elements) {
+        // todo: set
+    }
+
+    @Override
+    public double[] get() {
+        // todo: get
+        return new double[0];
+    }
+
+    @Override
+    public ArrayVector clone() {
+        // todo: clone
+
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        // todo: getSize
+
+        return 0;
+    }
+
+    @Override
+    public void set(int index, double value) {
+        // todo: set
+
+    }
+
+    @Override
+    public double get(int index) throws ArrayIndexOutOfBoundsException {
+        // todo: get
+
+        return 0;
+    }
+
+    @Override
+    public double getMax() {
+        // todo: getMax
+
+        return 0;
+    }
+
+    @Override
+    public double getMin() {
+        // todo: getMin
+
+        return 0;
+    }
+
+    @Override
+    public void sortAscending() {
+        // todo: sortAscending
+
+    }
+
+    @Override
+    public void mult(double factor) {
+        // todo: mult
+    }
+
+    @Override
+    public ArrayVector sum(ArrayVector anotherVector) {
+        // todo: sum
+        return null;
+    }
+
+    @Override
+    public double scalarMult(ArrayVector anotherVector) {
+        // todo: scalarMult
+        return 0;
+    }
+
+    @Override
+    public double getNorm() {
+        // todo: getNorm
+        return 0;
+    }
+}
---
+++ ArrayVector/src/ru/skillbench/tasks/basics/math/Main.java
@@ -0,0 +1,8 @@
+package ru.skillbench.tasks.basics.math;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello? I'm a Vector");
+    }
+}
