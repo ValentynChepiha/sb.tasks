bd6d00af87ea3001344fc135f3a6ef6a4ee106a2
Valentyn Chepiha <valentin.chepiga@gmail.com>
2021-11-25 12:53:13 +0200

    Implemented interface ArrayVector

--- ArrayVector/src/ru/skillbench/tasks/basics/math/ArrayVectorImpl.java
+++ ArrayVector/src/ru/skillbench/tasks/basics/math/ArrayVectorImpl.java
@@ -1,84 +1,118 @@
 package ru.skillbench.tasks.basics.math;
 
+import java.util.Arrays;
+
 public class ArrayVectorImpl implements ArrayVector {
+    private double[] vector;
+
+    public ArrayVectorImpl() {
+        vector = new double [0];
+    }
+
+    public ArrayVectorImpl(double... elements) {
+        vector = copyArray(elements);
+    }
+
+    private double[] copyArray(double[] elements){
+        int length = elements.length;
+        double[] newVector = new double[length];
+        System.arraycopy(elements, 0, newVector, 0, elements.length);
+        return newVector;
+    }
+
     @Override
     public void set(double... elements) {
-        // todo: set
+        vector = copyArray(elements);
     }
 
     @Override
     public double[] get() {
-        // todo: get
-        return new double[0];
+        return vector;
     }
 
     @Override
     public ArrayVector clone() {
-        // todo: clone
-
-        return null;
+        return new ArrayVectorImpl(vector);
     }
 
     @Override
     public int getSize() {
-        // todo: getSize
-
-        return 0;
+        return vector.length;
     }
 
     @Override
-    public void set(int index, double value) {
-        // todo: set
-
+    public void set(int index, double value)  {
+        if(index >= 0){
+            if(getSize()<=index){
+                double[] newVector = new double[index+1];
+                System.arraycopy(vector, 0, newVector, 0, getSize());
+                vector = newVector;
+            }
+            vector[index] = value;
+        }
     }
 
     @Override
     public double get(int index) throws ArrayIndexOutOfBoundsException {
-        // todo: get
-
-        return 0;
+        if(index <0 || index > getSize())
+            throw new ArrayIndexOutOfBoundsException();
+        return vector[index];
     }
 
     @Override
     public double getMax() {
-        // todo: getMax
-
-        return 0;
+        double max = Double.MAX_VALUE * -1;
+        for (double element : vector) {
+            max = Math.max(element, max);
+        }
+        return max;
     }
 
     @Override
     public double getMin() {
-        // todo: getMin
-
-        return 0;
+        double min = Double.MAX_VALUE;
+        for (double element : vector) {
+            min = Math.min(element, min);
+        }
+        return min;
     }
 
     @Override
     public void sortAscending() {
-        // todo: sortAscending
-
+        Arrays.sort(vector);
     }
 
     @Override
     public void mult(double factor) {
-        // todo: mult
+        for (int i=0; i<getSize(); i++) {
+            vector[i] = vector[i] * factor;
+        }
     }
 
     @Override
     public ArrayVector sum(ArrayVector anotherVector) {
-        // todo: sum
-        return null;
+        for(int i=0; i<getSize(); i++){
+            set(i, get(i) + (i<anotherVector.getSize() ? anotherVector.get(i) : 0.0));
+        }
+        return this;
     }
 
     @Override
     public double scalarMult(ArrayVector anotherVector) {
-        // todo: scalarMult
-        return 0;
+        int sizeMinVector = Math.min(getSize(), anotherVector.getSize());
+        double result = 0.0;
+        for (int i=0; i<sizeMinVector; i++ ){
+            result = result + get(i) * anotherVector.get(i);
+        }
+        return result;
     }
 
     @Override
     public double getNorm() {
-        // todo: getNorm
-        return 0;
+        double norm = 0.0;
+        for(int i=0; i<getSize(); i++){
+            norm = norm + Math.pow(get(i),2);
+        }
+        return Math.sqrt(norm);
     }
 }
